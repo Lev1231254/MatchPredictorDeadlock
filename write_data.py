12a9e6350ae5cb1046dfbed6cbc68dd4de1a977a
Leva <novikovlev48@gmail.com>
2026-06-12 11:16:28 +0200

    add safety_check to write_data

--- write_data.py
+++ write_data.py
@@ -2,20 +2,27 @@ import pandas as pd
 import pipeline.fetch_data as fetch_data
 import pipeline.preprocess as preprocess
 
-# save heroes table
-heroes = fetch_data.get_heroes_dataframe()
-heroes.to_csv("data/heroes.csv")
+
+safety_check = input("Saving datasets will take some time. Do you still want do it? (Y/n)\n")
+if safety_check == "Y":
+        print("Saving datasets into data/")
+        # save heroes table
+        heroes = fetch_data.get_heroes_dataframe()
+        heroes.to_csv("data/heroes.csv")
 
 
-# save preprocessed matches
-LIMIT = 1000
+        # save preprocessed matches
+        LIMIT = 1000
 
-features = '''match_id, match_outcome, winning_team, hero_id, team, 
-        match_mode, average_badge_team0, "stats.time_stamp_s", "stats.net_worth", 
-        "mid_boss.destroyed_time_s", "mid_boss.team_claimed"'''
-time_stamps = [500, 1000, 1500]
+        features = '''match_id, match_outcome, winning_team, hero_id, team, 
+                match_mode, average_badge_team0, "stats.time_stamp_s", "stats.net_worth", 
+                "mid_boss.destroyed_time_s", "mid_boss.team_claimed"'''
+        time_stamps = [500, 1000, 1500]
 
-for time_stamp in time_stamps:
-    data = fetch_data.get_match_dataframe(features, LIMIT)
-    data_preprocessed = preprocess.data_feature_preprocess(data, heroes, time_stamp)
-    data_preprocessed.to_csv("data/matches" + str(time_stamp) + ".csv", index=False)
+        for time_stamp in time_stamps:
+                data = fetch_data.get_match_dataframe(features, LIMIT)
+                data_preprocessed = preprocess.data_feature_preprocess(data, heroes, time_stamp)
+                data_preprocessed.to_csv("data/matches" + str(time_stamp) + ".csv", index=False)
+        print("Datasets are saved")
+else:
+        print("The programm is closed")
